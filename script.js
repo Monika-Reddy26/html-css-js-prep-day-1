2d27817ff7872b633d58257ab31ea1ebc1fbfef0
Monika S Reddy <monikasreddy26@gmail.com>
2026-02-04 19:51:41 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("completebtn")
+const message = document.getElementById("statusmessage")
+button.addEventListener("click", function () {
+  message.textContent = "Great job!You successfully completed day 1."
+  button.disabled = true;
+  button.textContent = "Completed";
+})
